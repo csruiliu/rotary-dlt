600ca15c61dda14ea147237cbe35b98996438132
Rui Liu <rui.liu@gmx.com>
2019-06-11 21:03:45 -0500

    update collection

--- mt-schedule/dnn_model.py
+++ mt-schedule/dnn_model.py
@@ -2,24 +2,28 @@ import sys
 
 
 class DnnModel(object):
-    def __init__(self, model_name, intance_name, model_layer, input_w, input_h, num_classes, batch_size_range, desired_accuracy):
+    def __init__(self, model_name, intance_name, model_layer, input_w, input_h, num_classes, batch_size, desired_accuracy):
 
         self.modelName = model_name
+        self.instanceName = model_name + intance_name
         self.modelLayer = model_layer
         self.inputWidth = input_w
         self.inputHeight = input_h
-        self.num_classes = num_classes
-        self.batchSizeRange = batch_size_range
+        self.numClasses = num_classes
+        self.batchSize = batch_size
         self.desiredAccuracy = desired_accuracy
         import_model = __import__(self.modelName)
         
         #the imported module and the regarding class name have to be the same
         clazz = getattr(import_model, self.modelName)
-        self.modelEntity = clazz(self.modelName + intance_name, self.modelLayer, self.inputWidth, self.inputHeight, self.num_classes)
+        self.modelEntity = clazz(self.instanceName, self.modelLayer, self.inputWidth, self.inputHeight, self.numClasses)
         
 
     def getModelLayer(self):
         return self.modelLayer
+
+    def getInstanceName(self):
+        return self.instanceName
 
     def getModelName(self):
         return self.modelName
@@ -27,8 +31,8 @@ class DnnModel(object):
     def getModelEntity(self):
         return self.modelEntity
 
-    def getBatchSizeRange(self):
-        return self.batchSizeRange
+    def getBatchSize(self):
+        return self.batchSize
 
     def getDesiredAccuracy(self):
         return self.desiredAccuracy
--- mt-schedule/schedule_demo.py
+++ mt-schedule/schedule_demo.py
@@ -14,16 +14,17 @@ img_w = 224
 img_h = 224
 num_channel = 3
 num_classes = 1000
-num_images = 10 
+num_images = 10
 
 modelCollection = []
 modelEntityCollection = []
 logitCollection = []
 scheduleCollection = []
+batchCollection = []
 
 features = tf.placeholder(tf.float32, [None, img_w, img_h, num_channel])
 labels = tf.placeholder(tf.int64, [None, num_classes])
-    
+
 #cifar10_dir = '/home/ruiliu/Development/mtml-tf/dataset/cifar-10'
 #cifar = cifar_utils(cifar10_dir)
 #X_data, _, Y_data = cifar.load_evaluation_data()
@@ -41,10 +42,13 @@ Y_data = load_labels_onehot(label_path)
 
 model_class_num = [3, 2, 1]
 model_class_total = sum(model_class_num)
-model_name_abbr = np.random.choice(100000, model_class_total, replace=False).tolist()
 
 model_class = ["resnet", "mobilenet", "perceptron"]
 all_batch_list = [10, 20, 40, 50, 80, 100]
+batch_size_total = sum(all_batch_list)
+
+model_name_abbr = np.random.choice(100000, model_class_total*batch_size_total, replace=False).tolist()
+
 
 def prepareModels():
     for idx, mls in enumerate(model_class):
@@ -52,37 +56,54 @@ def prepareModels():
             batch_num = np.random.randint(1, len(all_batch_list))
             batch_set = np.random.choice(all_batch_list, size=batch_num, replace=False)  
             layer_num = np.random.randint(1, 10)
-            
-            dm = DnnModel(mls, str(model_name_abbr.pop()), model_layer=layer_num, input_w=img_w, input_h=img_h,  
-                            num_classes=num_classes, batch_size_range=batch_set, desired_accuracy=0.9)
-            modelCollection.append(dm)
-    
+            for batch_size in batch_set:
+                dm = DnnModel(mls, str(model_name_abbr.pop()), model_layer=layer_num, input_w=img_w, input_h=img_h,  
+                            num_classes=num_classes, batch_size=batch_size, desired_accuracy=0.9)
+                modelCollection.append(dm)
+
+def saveModelDes():
+    with open('models_des.txt', 'a') as file:
+        file.write("Workload:\n")
+        for idx in modelCollection:
+            modelName = idx.getModelName()
+            modelLayer = idx.getModelLayer()
+            instanceName = idx.getInstanceName()
+            batchSize = idx.getBatchSize()
+            file.write('model type:' + modelName + '\n')
+            file.write('instance name:' + instanceName + '\n')
+            file.write('layer num: ' + str(modelLayer) + '\n')
+            file.write('input batch size: '+ str(batchSize)+ '\n')
+            file.write('=======================\n')
+
 def buildModels():
     for midx in modelCollection:
         modelEntity = midx.getModelEntity()
         modelEntityCollection.append(modelEntity) 
         modelLogit = modelEntity.build(features)
-        logitCollection.append(modelLogit)
+        logitUnit = []
+        logitUnit.append(modelLogit)
+        logitUnit.append(midx.getBatchSize())
+        logitCollection.append(logitUnit)
 
-    for lidx in modelEntityCollection:
-        print(lidx.getModelInstanceName())
-        print(lidx.getModelMemSize())
-
+    #for lidx in logitCollection:
+    #    print(lidx[1])
+    #    print(lidx.getModelMemSize())
 
 def scheduleNaive():
     for lit in logitCollection:
-        scheduleUnit = []
-        scheduleUnit.append(lit)
-        scheduleCollection.append(lit)
+        schUnit = []
+        schUnit.append(lit[0])
+        schUnit.append(lit[1])
+        scheduleCollection.append(schUnit)
+    
 
-
-def executeSch(sch_unit, X_train, Y_train):
+def executeSch(sch_unit, batch_unit, X_train, Y_train):
     total_time = 0
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     with tf.Session(config=config) as sess:
         sess.run(tf.global_variables_initializer())
-        mini_batches = 10
+        mini_batches = batch_unit    
         num_batch = Y_train.shape[0] // mini_batches            
         for i in range(num_batch):
             print('step %d / %d' %(i+1, num_batch))
@@ -96,11 +117,12 @@ def executeSch(sch_unit, X_train, Y_train):
 
 if __name__ == '__main__':
     prepareModels()
+    saveModelDes()
     buildModels()
     scheduleNaive()
     
     for sit in scheduleCollection:
-        p = Process(target=executeSch, args=(sit, X_data, Y_data,))
+        p = Process(target=executeSch, args=(sit[0], sit[1], X_data, Y_data,))
         p.start()
         print(p.pid)
         p.join()
