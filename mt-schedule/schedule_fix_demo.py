756effb0bf11f03c94976b4577e313562a233916
Rui Liu <rui.liu@gmx.com>
2019-06-14 11:17:01 -0500

    add test demo

--- mt-schedule/schedule_fix_demo.py
+++ mt-schedule/schedule_fix_demo.py
@@ -39,25 +39,6 @@ label_path = '/home/ruiliu/Development/mtml-tf/dataset/imagenet1k-label.txt'
 X_data = unpickle_load_images(bin_dir, num_images, num_channel, img_w, img_h)
 Y_data = load_labels_onehot(label_path)
 
-def prepareModels():
-    model_class_num = [3, 2, 1]
-    model_class_total = sum(model_class_num)
-
-    model_class = ["resnet", "mobilenet", "perceptron"]
-    all_batch_list = [10, 20, 40, 50, 80, 100]
-    batch_size_total = sum(all_batch_list)
-
-    model_name_abbr = np.random.choice(100000, model_class_total*batch_size_total, replace=False).tolist()
-    for idx, mls in enumerate(model_class):
-        for _ in range(model_class_num[idx]):
-            batch_num = np.random.randint(1, len(all_batch_list))
-            batch_set = np.random.choice(all_batch_list, size=batch_num, replace=False)  
-            layer_num = np.random.randint(1, 10)
-            for batch_size in batch_set:
-                dm = DnnModel(mls, str(model_name_abbr.pop()), model_layer=layer_num, input_w=img_w, input_h=img_h,  
-                            num_classes=num_classes, batch_size=batch_size, desired_accuracy=0.9)
-                modelCollection.append(dm)
-
 def prepareModelsFix():
     model_class_num = [2, 4, 3, 1]
     model_class = ["resnet", "mobilenet", "perceptron", "convnet"]
@@ -70,6 +51,20 @@ def prepareModelsFix():
                             num_classes=num_classes, batch_size=all_batch_list.pop(), desired_accuracy=0.9)
             modelCollection.append(dm)
 
+def prepareMobilenetFix():
+    model_class_num = [8]
+    model_class = ["mobilenet"]
+    
+    layer_list = [5, 2, 8, 4, 1, 1, 1, 1, 1, 1]
+    model_name_abbr = np.random.choice(100000, 10, replace=False).tolist()    
+    
+    all_batch_list = [200, 125, 100, 80, 50, 40, 20, 10]
+
+    for idx, mls in enumerate(model_class):
+        for _ in range(model_class_num[idx]):
+            dm = DnnModel(mls, str(model_name_abbr.pop()), model_layer=layer_list.pop(), input_w=img_w, input_h=img_h,  
+                            num_classes=num_classes, batch_size=all_batch_list.pop(), desired_accuracy=0.9)
+            modelCollection.append(dm)
 
 def saveModelDes():
     with open('models_des.txt', 'a') as file:
@@ -95,11 +90,18 @@ def buildModels():
         logitUnit.append(midx.getBatchSize())
         logitCollection.append(logitUnit)
 
-    #for lidx in logitCollection:
-    #    print(lidx[1])
-    #    print(lidx.getModelMemSize())
-
-def scheduleGreedy():
+def scheduleNo():
+    for lit in logitCollection:
+        logitUnit = []
+        logitUnit.append(lit[0])
+        batchUnit = []
+        batchUnit.append(lit[1])
+        schUnit = []
+        schUnit.append(logitUnit)
+        schUnit.append(batchUnit)
+        scheduleCollection.append(schUnit)
+                        
+def scheduleFix():
     schUnit1 = []
     logitUnit1 = []
     batchUnit1 = []
@@ -144,15 +146,6 @@ def scheduleGreedy():
     schUnit4.append(batchUnit4)
     scheduleCollection.append(schUnit4)
 
-    
-
-def scheduleNaive():
-    for lit in logitCollection:
-        schUnit = []
-        schUnit.append(lit[0])
-        schUnit.append(lit[1])
-        scheduleCollection.append(schUnit)
-
 
 def executeSch(sch_unit, batch_unit, X_train, Y_train):
     total_time = 0
@@ -190,12 +183,10 @@ def executeSch(sch_unit, batch_unit, X_train, Y_train):
                     print("training time for 1 epoch:", total_time)  
 
 if __name__ == '__main__':
-    #prepareModels()
     prepareModelsFix()
     saveModelDes()
     buildModels()
-    #scheduleNaive()
-    scheduleGreedy()
+    scheduleNo()
 
     for sit in scheduleCollection:
         p = Process(target=executeSch, args=(sit[0], sit[1], X_data, Y_data,))
