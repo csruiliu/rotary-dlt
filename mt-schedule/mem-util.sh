2693701c7bcdcd0250ad3df3e24cb49a1874c55d
csruiliu <rui.liu@gmx.com>
2019-06-25 12:17:02 -0500

    update mem-util script

--- mt-schedule/mem-util.sh
+++ mt-schedule/mem-util.sh
@@ -1,24 +1,10 @@
-checkret()
-{
-    while true
-    do
-        if [ $1 != 100 ]
-        then
-            exit 0
-        fi
-    done
-}
-
 datetime=$(date +%Y%m%d%H%M)
 timestamp=$(date +%Y%m%d%H%M%S.%3N)
-echo "Expertiment starts $timestamp" > /home/ruiliu/Development/mtml-tf/mt-schedule/mem-monitor-$datetime.csv
-python3 benchmark_packed.py & nvidia-smi --query-gpu=timestamp,utilization.memory --format=csv --loop-ms=100 >> /home/ruiliu/Development/mtml-tf/mt-schedule/mem-monitor-$datetime.csv
+python3 benchmark_packed.py & 
+nvidia-smi --query-gpu=utilization.memory --format=csv --loop-ms=100 >> /home/ruiliu/Development/mtml-tf/mt-schedule/mem-monitor-$datetime.csv & 
+wait -n
+pkill -P $$
 
-#RET=100
-#checkret $RET & nvidia-smi --query-gpu=timestamp,utilization.memory --format=csv --loop-ms=100 >> /home/ruiliu/Development/mtml-tf/mt-schedule/mem-monitor-$datetime.txt & python3 benchmark_packed.py ; RET=$? 
-
-
-#
 #if [ $RET -eq 0 ]
 #then
 #    exit 0
@@ -26,3 +12,20 @@ python3 benchmark_packed.py & nvidia-smi --query-gpu=timestamp,utilization.memor
 #    echo "return is wrong"
 #    exit 1
 #checkprocess $MYPID
+
+#checkret()
+#{
+#    while true
+#    do
+#        if [ $1 != 100 ]
+#        then
+#            exit 0
+#        fi
+#    done
+#}
+
+#echo "Expertiment starts $timestamp" > /home/ruiliu/Development/mtml-tf/mt-schedule/mem-monitor-$datetime.csv
+#python3 benchmark_packed.py & nvidia-smi --query-gpu=timestamp,utilization.memory --format=csv --loop-ms=100 >> /home/ruiliu/Development/mtml-tf/mt-schedule/mem-monitor-$datetime.csv
+
+#RET=100
+#checkret $RET & nvidia-smi --query-gpu=utilization.memory --format=csv --loop-ms=100 >> /home/ruiliu/Development/mtml-tf/mt-schedule/mem-monitor-$datetime.csv & python3 benchmark_packed.py && RET=$? 
