89b1f781a374dfef824cc3799bd38607f5cf8b73
Rui Liu <rui.liu@gmx.com>
2019-07-12 23:36:59 -0500

    add mem_fb plot

--- figure/mem_fb.py
+++ figure/mem_fb.py
@@ -15,14 +15,15 @@ args = parser.parse_args()
 csvpath = args.csvfile
 #outpath = args.outfile
 outpath = csvpath.replace('.csv','.png')
+expName = csvpath.split('.')[0]
 
 y = []
 count = 1
 with open(csvpath,'r') as csvfile:
     next(csvfile)
-    read = csv.reader(csvfile, delimiter=',')
+    read = csv.reader(csvfile, delimiter=' ')
     for row in read:
-        y.append(int(re.sub('%', '', row[0])))
+        y.append(int(row[0]))
         count += 1
 
 x = np.arange(1, count)
@@ -31,10 +32,10 @@ ax = plt.axes()
 ax.tick_params(direction='in')
 ax.grid(linestyle='--')
 plt.plot(x,y)
-plt.title('GPU Memory Monitor for DNN Models')
-plt.yticks(np.arange(0,101,20), ('0%', '20%', '40%', '60%', '80%', '100%'))
+plt.title(expName)
+plt.yticks(np.arange(0,16278,3200), ('0%', '20%', '40%', '60%', '80%', '100%'))
 plt.xlabel("Total Training Time (ms)")
-plt.ylabel("GPU Memory Utilization (%)")
+plt.ylabel("GPU Free Memory")
 #plt.show()
 plt.savefig(outpath,format='png')
 
---
+++ figure/mem_util.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import re
+
+import argparse
+parser = argparse.ArgumentParser()
+#parser.add_argument('-f', '--csvfile', type=str, help='identify a csv file to figure')
+parser.add_argument("csvfile", help="identify a csv file to figure")
+#parser.add_argument("outfile", help="identify the output filename")
+args = parser.parse_args()
+
+csvpath = args.csvfile
+#outpath = args.outfile
+outpath = csvpath.replace('.csv','.png')
+expName = csvpath.split('.')[0]
+
+y = []
+count = 1
+with open(csvpath,'r') as csvfile:
+    next(csvfile)
+    read = csv.reader(csvfile, delimiter=',')
+    for row in read:
+        y.append(int(re.sub('%', '', row[0])))
+        count += 1
+
+x = np.arange(1, count)
+
+ax = plt.axes()
+ax.tick_params(direction='in')
+ax.grid(linestyle='--')
+plt.plot(x,y)
+plt.title(expName)
+plt.yticks(np.arange(0,101,20), ('0%', '20%', '40%', '60%', '80%', '100%'))
+plt.xlabel("Total Training Time (ms)")
+plt.ylabel("GPU Memory Utilization (%)")
+#plt.show()
+plt.savefig(outpath,format='png')
+
