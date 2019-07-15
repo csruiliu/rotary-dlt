a2c9111a8bba9f8c6449833a63ce269723ed28f9
Rui Liu <rui.liu@gmx.com>
2019-07-15 15:28:21 -0500

    add tf profiler

--- mt-perf/perf_packed.py
+++ mt-perf/perf_packed.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
+
+from tensorflow.python.profiler import model_analyzer
+from tensorflow.python.profiler import option_builder
+
 import numpy as np
 from multiprocessing import Process
 
@@ -38,7 +42,7 @@ trainCollection = []
 scheduleCollection = []
 batchCollection = []
 
-input_model_num = 5
+input_model_num = 1
 
 #bin_dir = '/home/ruiliu/Development/mtml-tf/dataset/imagenet1k.bin'
 #label_path = '/home/ruiliu/Development/mtml-tf/dataset/imagenet1k-label.txt'
@@ -125,8 +129,16 @@ def executePack(train_collection, num_epoch, X_train, Y_train):
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     config.allow_soft_placement = True
+    
+    #builder = tf.profiler.ProfileOptionBuilder
+    #opts = builder(builder.time_and_memory()).order_by('micros').build()
+
+    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    run_metadata = tf.RunMetadata()
+    #with tf.contrib.tfprof.ProfileContext('./train_dir', trace_steps=range(0, 2), dump_steps=range(0, 2)) as pctx:
     with tf.Session(config=config) as sess:
         sess.run(tf.global_variables_initializer())
+        model_profiler = model_analyzer.Profiler(graph=sess.graph)
         num_batch = Y_train.shape[0] // batchSize
         num_batch_list = np.arange(num_batch)
         for e in range(num_epoch):
@@ -139,7 +151,9 @@ def executePack(train_collection, num_epoch, X_train, Y_train):
                         names['Y_mini_batch_feed' + str(ridx)] = Y_train[rand_idx:rand_idx+batchSize,:]
                         input_dict[names['features' + str(ridx)]] = names['X_mini_batch_feed' + str(ridx)]
                         input_dict[names['labels' + str(ridx)]] = names['Y_mini_batch_feed' + str(ridx)]
-                    sess.run(train_collection, feed_dict=input_dict)
+                    #sess.run(train_collection, feed_dict=input_dict)
+                    sess.run(train_collection, feed_dict=input_dict, options=run_options, run_metadata=run_metadata)
+                    model_profiler.add_step(step=i, run_meta=run_metadata)
                 else:
                     if isShuffle:
                         rand_idx = int(np.random.choice(num_batch_list, 1, replace=False))
@@ -148,8 +162,14 @@ def executePack(train_collection, num_epoch, X_train, Y_train):
                     else:
                         X_mini_batch_feed = X_train[i:i+batchSize,:,:,:]
                         Y_mini_batch_feed = Y_train[i:i+batchSize,:]
-                    sess.run(train_collection, feed_dict={features: X_mini_batch_feed, labels: Y_mini_batch_feed})
-
+                    #sess.run(train_collection, feed_dict={features: X_mini_batch_feed, labels: Y_mini_batch_feed})
+                    sess.run(train_collection, feed_dict={features: X_mini_batch_feed, labels: Y_mini_batch_feed}, options=run_options, run_metadata=run_metadata)
+                    model_profiler.add_step(step=i, run_meta=run_metadata)
+        
+        profile_graph_opts_builder = option_builder.ProfileOptionBuilder(option_builder.ProfileOptionBuilder.time_and_memory())
+        profile_graph_opts_builder.with_timeline_output(timeline_file='/tank/ruiliu/mtml-tf/mt-perf/profile_dir/mobile-m1-b10/model_profiler.json')
+        #profile_graph_opts_builder.with_timeline_output(timeline_file='/home/ruiliu/Development/mtml-tf/mt-perf/profile_dir/mobile-m5-b10/model_profiler.json')
+        model_profiler.profile_graph(profile_graph_opts_builder.build())
 
 def executePackCombine(train_collection_combine, num_epoch, X_train, Y_train):
     config = tf.ConfigProto()
@@ -170,8 +190,7 @@ def executePackCombine(train_collection_combine, num_epoch, X_train, Y_train):
                     X_mini_batch_feed = X_train[i:i+batchSize,:,:,:]
                     Y_mini_batch_feed = Y_train[i:i+batchSize,:]
                 sess.run(train_collection_combine, feed_dict={features: X_mini_batch_feed, labels: Y_mini_batch_feed})
-
-
+    
 if __name__ == '__main__':
     deviceId = '/device:GPU:' + str(gpuId)
     print("run gpu:", deviceId)
