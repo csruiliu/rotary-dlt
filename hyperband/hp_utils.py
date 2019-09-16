842b83de60fc6fe0421d8bb83bcbb738c53f3fb1
Rui Liu <rui.liu@gmx.com>
2019-09-15 23:59:50 -0500

    finish hyperband for dry run

---
+++ hyperband/hp_utils.py
@@ -0,0 +1,19 @@
+import random
+import numpy as np
+import itertools
+from operator import itemgetter 
+
+def init_hp(n_conf):
+	batch_size = np.arange(0,60,5)
+	batch_size[0] = 1
+	opt_conf = ['Adam','SGD']
+	data_conf = ['Same','Diff']
+	preprocess_list = ['Include','Not Include']
+	all_conf = [batch_size,opt_conf,data_conf,preprocess_list]
+	hp_conf = list(itertools.product(*all_conf))
+	idx_list = np.random.choice(np.arange(0, len(hp_conf)), n_conf)
+	rand_conf = itemgetter(*idx_list)(hp_conf)
+	
+	return rand_conf
+	#print(rand_conf)
+	#print(type(hp_conf)
--- hyperband/hyperband.py
+++ hyperband/hyperband.py
@@ -1,9 +1,93 @@
 import numpy as np
-
-from random import random
-from math import log, ceil
-from time import time, ctime
+from math import log, ceil, floor
+from timeit import default_timer as timer
+import hp_utils
 
 class Hyperband:
-	def __init__(self, get_params_function, try_params_function):
+	def __init__(self):
+		# maximun budget for single configuration, i.e., maximum iterations per configuration in example
+		self.R = 81
+		# defines configuration downsampling rate (default = 3)
+		self.eta = 3		
+		# control how many runs
+		self.s_max = floor(log(self.R, self.eta))
+		# maximun budget for all configurations
+		self.B = (self.s_max + 1) * self.R
+		# list of dicts
+		self.results = []	
+		self.counter = 0
+		self.best_loss = np.inf
+		self.best_counter = -1
 
+	def run(self, skip_last=0, dry_run=True):
+		for s in reversed(range(self.s_max + 1)):
+			n = ceil(self.B / self.R / (s + 1) * (self.eta ** s))
+			r = self.R * self.eta ** (-s)
+			T = hp_utils.init_hp(n)
+			
+			for i in range(s + 1):
+				n_i = floor(n * self.eta ** (-i))
+				r_i = int(r * self.eta ** (i))
+				print("\n*** {} bracket | {} configurations x {} iterations each ***".format(s, n_i, r_i))
+				print("==============================================================")
+
+				val_losses = []
+				#early_stops = []
+
+				for t in T:
+					self.counter += 1
+					
+					
+					start_time = timer()
+
+					if dry_run:
+						result = {'loss':np.random.random(),'auc':np.random.random()}
+					#else:
+						#result = self.try_params(n_iterations, t)	
+					
+					# check the results format and if loss is generated 
+					assert(type(result) == dict)
+					assert('loss' in result)
+					
+					end_time = timer()
+
+					seconds = end_time - start_time
+					#print("\n{} seconds.".format(seconds))
+
+					loss = result['loss']	
+					val_losses.append(loss)
+					
+					if loss < self.best_loss:
+						self.best_loss = loss
+						self.best_counter = self.counter
+
+					print("current run {}, loss: {:.5f} | lowest loss so far: {:.5f} (run {})\n".format(self.counter, loss, self.best_loss, self.best_counter))
+
+					result['counter'] = self.counter
+					result['seconds'] = seconds
+					result['params'] = t
+					# r_i is the resource, i.e., epochs to run the current configuration t in the example
+					result['resources'] = r_i
+
+					self.results.append(result)
+
+				indices = np.argsort(val_losses)
+				
+				T = [T[i] for i in indices]
+				T = T[0:floor(n_i / self.eta)]
+				
+				#print(indices)
+
+				#T = T[0:int(n_i / self.eta)]
+		
+		#print("best result=", self.best_loss)
+		#print("best counter=", self.best_counter)
+		return self.results
+
+if __name__ == "__main__":
+	#hp_utils.init_hp(10)
+	hb = Hyperband()
+	results = hb.run()
+	print("{} total, best:\n".format(len(results)))
+	best_hp = sorted(results, key = lambda x: x['loss'])[0]
+	print(best_hp)
