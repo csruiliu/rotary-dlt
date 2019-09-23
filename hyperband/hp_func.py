c49b4741dae79ba4f9544be12d0c424ff1fddd04
Rui Liu <rui.liu@gmx.com>
2019-09-22 21:26:04 -0500

    add evaluate

--- hyperband/hp_func.py
+++ hyperband/hp_func.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
-import random
 import numpy as np
 import itertools
 from operator import itemgetter
 from datetime import datetime
 import sys
 
-from img_utils import *
-from mlp import *
+from img_utils import load_images_bin, load_labels_onehot, check_image_chn
+from mobilenet import MobileNet
+
 
 imgWidth = 224
 imgHeight = 224
@@ -19,9 +19,8 @@ bin_dir = '/home/ruiliu/Development/mtml-tf/dataset/imagenet1k.bin'
 #label_path = '/tank/local/ruiliu/dataset/imagenet10k-label.txt'
 label_path = '/home/ruiliu/Development/mtml-tf/dataset/imagenet1k-label.txt'
 
-def init_hp(n_conf):
-    batch_size = np.arange(10,31,10)
-    #batch_size[0] = 1
+def get_params(n_conf):
+    batch_size = np.arange(10,61,5)
     opt_conf = ['Adam','SGD']
     data_conf = ['Same','Diff']
     preprocess_list = ['Include','Not Include']
@@ -32,8 +31,9 @@ def init_hp(n_conf):
 
     return rand_conf
 
-
 def run_params(hyper_params, iterations, conn):
+    return None
+    '''
     features = tf.placeholder(tf.float32, [None, imgWidth, imgHeight, numChannels])
     labels = tf.placeholder(tf.int64, [None, numClasses])
     X_data = load_images_bin(bin_dir, numChannels, imgWidth, imgHeight)
@@ -70,6 +70,49 @@ def run_params(hyper_params, iterations, conn):
         conn.send(acc_arg)
         conn.close()
         print("Accuracy:", acc_arg)
-        
+        '''
         #print("Accuracy:", evalOps.eval({features: X_data, labels: Y_data}))
+
+def evaluate_model():
+    features = tf.placeholder(tf.float32, [None, imgWidth, imgHeight, numChannels])
+    labels = tf.placeholder(tf.int64, [None, numClasses])
+
+    X_data = load_images_bin(bin_dir, numChannels, imgWidth, imgHeight)
+    Y_data = load_labels_onehot(label_path, numClasses)
+
+    dt = datetime.now()
+    np.random.seed(dt.microsecond)
+    net_instnace = np.random.randint(sys.maxsize)
+
+    batch_size = 32
+    opt = 'Adam'
+
+    modelEntity = MobileNet("mobilenet_"+str(net_instnace), 1, imgHeight, imgWidth, batch_size, numClasses, opt)
+    modelLogit = modelEntity.build(features)
+    trainOps = modelEntity.train(modelLogit, labels)
+    evalOps = modelEntity.evaluate(modelLogit, labels)
+    
+    iterations = 1
+
+    config = tf.ConfigProto()
+    config.allow_soft_placement = True
+
+    with tf.Session(config=config) as sess:
+        sess.run(tf.global_variables_initializer())
+        num_batch = Y_data.shape[0] // batch_size
+        for e in range(iterations):
+            for i in range(num_batch):
+                print('epoch %d / %d, step %d / %d' %(e+1, iterations, i+1, num_batch))
+                batch_offset = i * batch_size
+                batch_end = (i+1) * batch_size
+                X_mini_batch_feed = X_data[batch_offset:batch_end,:,:,:]
+                Y_mini_batch_feed = Y_data[batch_offset:batch_end,:]
+                sess.run(trainOps, feed_dict={features: X_mini_batch_feed, labels: Y_mini_batch_feed})
         
+        acc_arg = evalOps.eval({features: X_mini_batch_feed, labels: Y_mini_batch_feed})
+
+        #acc_arg = sess.run(evalOps, feed_dict={features: X_mini_batch_feed, labels: Y_mini_batch_feed})
+        
+        print("Accuracy:", acc_arg)
+
+
--- hyperband/hyperband.py
+++ hyperband/hyperband.py
@@ -1,31 +1,76 @@
 import numpy as np
 from multiprocessing import Process, Pipe
-#from multiprocessing import Pool
 from math import log, ceil, floor
 from timeit import default_timer as timer
-import hp_utils
+from hp_func import get_params, run_params, evaluate_model
+
+from img_utils import check_image_chn
 
 class Hyperband:
 
-    def __init__(self, getHyperPara, runHyperPara):
+    def __init__(self, resourceConf, downRate, getHyperPara, runHyperPara):
         # maximun budget for single configuration, i.e., maximum iterations per configuration in example
-        self.R = 27
+        self.R = resourceConf
         # defines configuration downsampling rate (default = 3)
-        self.eta = 3
+        self.eta = downRate
         # control how many runs
         self.s_max = floor(log(self.R, self.eta))
         # maximun budget for all configurations
         self.B = (self.s_max + 1) * self.R
-        # list of dicts
+        # list of results
         self.results = []
+        
         self.counter = 0
         self.best_acc = np.NINF
         self.best_counter = -1
+
         self.get_hyperparams = getHyperPara
         self.run_hyperParams = runHyperPara
+
+    def run_fake(self):
+        for s in reversed(range(self.s_max + 1)):
+            n = ceil(self.B / self.R / (s + 1) * (self.eta ** s))
+            r = self.R * (self.eta ** (-s))
+            T = self.get_hyperparams(n)
+
+            for i in range(s + 1):
+                n_i = floor(n * self.eta ** (-i))
+                r_i = int(r * self.eta ** (i))
+                print("\n*** {} bracket | {} configurations x {} iterations each ***".format(s, n_i, r_i))
+                print("==============================================================")
+
+                # record all accuracy of current run for sorting
+                val_acc = []
+
+                for t in T:
+                    result = {'acc':-1, 'counter':-1}
+                    self.counter += 1
+                    # generate random accuracy
+                    acc = np.random.random()
+                    val_acc.append(acc)
+
+                    result['acc'] = acc
+                    result['counter'] = self.counter
+                    result['params'] = t
+
+                    # record the best result
+                    if self.best_acc < acc:
+                        self.best_acc = acc
+                        self.best_counter = self.counter
+                    
+                    print("current run {}, accuracy: {:.5f} | best accuracy so far: {:.5f} (run {})\n".format(self.counter, acc, self.best_acc, self.best_counter))
+
+                    self.results.append(result)
+                    
+                # sort the result
+                indices = np.argsort(val_acc)
+                
+                T = [T[i] for i in indices]
+                T = T[0:floor(n_i / self.eta)]
         
-    def run(self, skip_last=0, dry_run=False):
-        
+        return self.results
+
+    def run(self):
         for s in reversed(range(self.s_max + 1)):
             n = ceil(self.B / self.R / (s + 1) * (self.eta ** s))
             r = self.R * self.eta ** (-s)
@@ -36,57 +81,51 @@ class Hyperband:
                 r_i = int(r * self.eta ** (i))
                 print("\n*** {} bracket | {} configurations x {} iterations each ***".format(s, n_i, r_i))
                 print("==============================================================")
-                
-                val_acc = []
+
+                # init list of accuracy of confs                
+                list_acc = []
 
                 for t in T:
                     result = {'acc':-1, 'counter':-1}
-                    self.counter += 1                    
-                    if dry_run:
-                        acc = np.random.random()
-                        result['acc'] = acc
-                    else:
-                        parent_conn, child_conn = Pipe()
-                        p = Process(target=self.run_hyperParams, args=(t, r_i, child_conn))
-                        p.start()
-                        acc = parent_conn.recv()
-                        result['acc'] = acc
-                        parent_conn.close()
-                        p.join()
+                    self.counter += 1
 
-                        #print(data)
-                        #child_conn.close()
-                        #result = {'acc':np.random.random()}
-                    
-                    # check the results format and if loss is generated
-                    #assert(type(result) == dict)
-                    #assert('acc' in result)
+                    # use process to run multipe models
+                    parent_conn, child_conn = Pipe()
+                    p = Process(target=self.run_hyperParams, args=(t, r_i, child_conn))
+                    p.start()
+                    acc = parent_conn.recv()
+                    result['acc'] = acc
+                    parent_conn.close()
+                    p.join()
 
-                    val_acc.append(acc)
-                    
+                    list_acc.append(acc)
+
                     if self.best_acc < acc:
                         self.best_acc = acc
                         self.best_counter = self.counter
-                    
+
                     result['counter'] = self.counter
                     result['params'] = t
-                    
+
                     print("current run {}, accuracy: {:.5f} | best accuracy so far: {:.5f} (run {})\n".format(self.counter, acc, self.best_acc, self.best_counter))
 
-                    # r_i is the resource, i.e., epochs to run the current configuration t in the example
-                    # result['resources'] = r_i
                     self.results.append(result)
-                    indices = np.argsort(val_acc)
+                
+                indices = np.argsort(list_acc)
 
                 T = [T[i] for i in indices]
                 T = T[0:floor(n_i / self.eta)]
 
-        return self.results
+            return self.results
 
 if __name__ == "__main__":
-	#hp_utils.init_hp(10)
-	hb = Hyperband(hp_utils.init_hp, hp_utils.run_params)
-	results = hb.run()
-	print("{} total, best:\n".format(len(results)))
-	best_hp = sorted(results, key = lambda x: x['acc'])[-1]
-	print(best_hp)
+    check_image_chn('/home/ruiliu/Development/mtml-tf/dataset/imagenet50k')
+    #evaluate_model()
+    #resource_conf = 27
+    #down_rate = 3
+    #hb = Hyperband(resource_conf, down_rate, get_params, run_params)
+    #results = hb.run_fake()
+    
+    #print("{} total, best:\n".format(len(results)))
+    #best_hp = sorted(results, key = lambda x: x['acc'])[-1]
+    #print(best_hp)
