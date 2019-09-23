3900943e0c4c04e8dca6b4a953dc10b67130bfde
Rui Liu <rui.liu@gmx.com>
2019-09-23 12:32:51 -0500

    update save and load

--- hyperband/img_utils.py
+++ hyperband/img_utils.py
@@ -28,12 +28,9 @@ def save_bin_raw(arr, output_file):
         of.write(arr)
 
 def load_bin_raw(img_bin):
-    with open(img_bin, mode='rb') as file:
-        raw_images = file.read()
-        buffer_images = np.frombuffer(raw_images, dtype=np.uint8)
-        float_images = np.array(buffer_images, dtype=float) / 255.0
-        img_num = int(float_images.size / imgWidth / imgHeight / numChannels)
-        images = float_images.reshape((img_num, imgWidth, imgHeight, numChannels))
+    image_arr = np.fromfile(img_bin, dtype=np.uint8)
+    img_num = int(image_arr.size / imgWidth / imgHeight / numChannels)
+    images = image_arr.reshape((img_num, imgWidth, imgHeight, numChannels))
     return images
 
 # use pickle package to store, but it is slow
@@ -51,30 +48,34 @@ def load_bin_pickle(path, num_channels, img_w, img_h):
     #raw_float = raw_float.transpose([0, 3, 1, 2])
     return raw_float
 
+# read single image
 def read_single_image(img_name, img_h, img_w):
     img = Image.open(img_name).convert("RGB")
     img_resize = img.resize((img_h, img_w), Image.NEAREST)
-    np_im = np.array(img_resize)
+    np_im = np.array(img_resize, dtype=np.uint8)
     img_ext = np.expand_dims(np_im, axis=0)
     return img_ext
 
 
 if __name__ == "__main__":
 
-    imgDir = '/home/ruiliu/Development/mtml-tf/dataset/imagenet1k'
-    outDir = '/home/ruiliu/Development/mtml-tf/dataset/imagenet1k.bin'
+    imgDir = '/home/ruiliu/Development/mtml-tf/dataset/imagenet10k'
+    outDir = '/home/ruiliu/Development/mtml-tf/dataset/imagenet10k.bin'
+
+    #imgDir = '/tank/local/ruiliu/dataset/imagenet10k'
+    #outDir = '/home/local/ruiliu/dataset/imagenet10k.bin'
 
     #bin_dir = '/tank/local/ruiliu/dataset/imagenet10k.bin'
     #bin_dir = '/home/ruiliu/Development/mtml-tf/dataset/imagenet1k.bin'
     #label_path = '/tank/local/ruiliu/dataset/imagenet10k-label.txt'
     #label_path = '/home/ruiliu/Development/mtml-tf/dataset/imagenet1k-label.txt'
     
-    arr_image = convert_image_bin(imgDir, imgWidth, imgHeight)
-    save_bin_raw(arr_image, outDir)
-    images = load_bin_raw('/home/ruiliu/Development/mtml-tf/dataset/imagenet1k.bin')
+    #arr_image = convert_image_bin(imgDir, imgWidth, imgHeight)
+    #save_bin_raw(arr_image, outDir)
+    images = load_bin_raw('/home/ruiliu/Development/mtml-tf/dataset/imagenet10k.bin')
     
     print(images.shape)
     #check_single_image(imgDir+'/ILSVRC2010_val_00001175.JPEG')
 
-    #plt.imshow(images[100,:,:,:])
-    #plt.show()
+    plt.imshow(images[100,:,:,:])
+    plt.show()
