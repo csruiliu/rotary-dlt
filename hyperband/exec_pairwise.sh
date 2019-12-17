94a47347601005e42e9852a22b90456970bf8f92
Rui Liu <rui.liu@gmx.com>
2019-12-17 13:06:23 -0600

    revise exec_pairwise for cleaning

--- hyperband/exec_pairwise.sh
+++ hyperband/exec_pairwise.sh
@@ -5,15 +5,16 @@ for i in $(seq 0 $END);
 do 
     touch 'exp_'$i.txt 
     SNG_A=`python3 pairwise_engine.py --single $i`  
-    `python3 clean_clean_gpu_cache.py`
+    python3 clean_clean_gpu_cache.py
+    rm -rf __pycache__
     for j in $(seq $i $END);
     do
         SNG_B=`python3 pairwise_engine.py --single $j`
-        `python3 clean_clean_gpu_cache.py`
+        python3 clean_clean_gpu_cache.py
+        rm -rf __pycache__
         PACK_AB=`python3 pairwise_engine.py --packmode --pack $i,$j` 
-        `python3 clean_clean_gpu_cache.py`
-        #SEQ=`$SNG_A + $SNG_B | bc`
-        #echo $SEQ
+        python3 clean_clean_gpu_cache.py
+        rm -rf __pycache__
         echo "scale=4; ($SNG_A+$SNG_B-$PACK_AB)/($SNG_A+$SNG_B)" | bc >> 'exp_'$i.txt 
     done
 done
