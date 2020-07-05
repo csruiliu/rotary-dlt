49d7b4271866e44d589eeb77f88af6ed2e01c413
csruiliu <rui.liu@gmx.com>
2020-07-05 12:50:14 -0500

    use sigmoid function to estimate the accuracy

--- device-placement/model_accuracy_estimator.py
+++ device-placement/model_accuracy_estimator.py
@@ -1,9 +1,12 @@
 import numpy as np
-from scipy.optimize import minimize
-from scipy.special import factorial
-from scipy import stats
+from scipy.optimize import curve_fit
 
 import model_accuracy_config as acc_json_cfg
+
+
+def sigmoid_function(x, x0, k):
+    y = 1 / (1 + np.exp(-k * (x - x0)))
+    return y
 
 
 def jaccard_index(s1, s2):
@@ -23,6 +26,7 @@ def jaccard_index(s1, s2):
 def generate_conv_model_dataset():
     conv_model_dataset = list()
     conv_model_accuracy_list = list()
+    conv_model_epoch_list = list()
     for model_info in acc_json_cfg.conv_model_list:
         conv_model_info_dict = dict()
         conv_model_info_list = model_info['model_name'].split('-')
@@ -36,12 +40,12 @@ def generate_conv_model_dataset():
         conv_model_info_dict['learning_rate'] = float(conv_model_info_list[6])
         conv_model_info_dict['optimizer'] = conv_model_info_list[7]
         conv_model_info_dict['activation'] = conv_model_info_list[8]
-        conv_model_info_dict['epochs'] = int(conv_model_info_list[9])
         conv_model_dataset.append(conv_model_info_dict)
 
+        conv_model_epoch_list.append(int(conv_model_info_list[9]))
         conv_model_accuracy_list.append(float(model_info['model_accuracy']))
 
-    return conv_model_dataset, conv_model_accuracy_list
+    return conv_model_dataset, conv_model_accuracy_list, conv_model_epoch_list
 
 
 def compute_model_similarity(center_model, candidate_models):
@@ -55,7 +59,7 @@ def compute_model_similarity(center_model, candidate_models):
         input_binary_set = set()
         candidate_binary_set = set()
         for cfg_idx in center_model:
-            if cfg_idx == 'channel_num' or cfg_idx == 'optimizer' or cfg_idx == 'activation':
+            if cfg_idx in ('channel_num', 'optimizer', 'activation'):
                 #input_size_similarity = 1 if center_model[cfg_idx] == model_idx[cfg_idx] else 0
                 #curmodel_hyperparam_similarity_list.append(input_size_similarity)
                 input_binary_set.add(center_model[cfg_idx])
@@ -75,53 +79,30 @@ def compute_model_similarity(center_model, candidate_models):
     return candidate_models_similarity_list
 
 
-def rank_model_similarity(candidate_models_info_list, candidate_models_accuracy_list, candidate_models_similarity_list, k=2):
+def rank_model_similarity(candidate_models_info_list, candidate_models_similarity_list, k=2):
     assert len(candidate_models_info_list) >= k, 'the number of expected selected models is larger than the candidate models'
 
-    similarity_sorted_idx_list = sorted(range(len(candidate_models_similarity_list)), key=lambda k: candidate_models_similarity_list[k], reverse=True)[:k]
-    # return the nearest models according to similarity
-    topk_model_info_list = [candidate_models_info_list[i] for i in similarity_sorted_idx_list]
-    topk_model_accuracy_list = [candidate_models_accuracy_list[i] for i in similarity_sorted_idx_list]
+    similarity_sorted_idx_list = sorted(range(len(candidate_models_similarity_list)),
+                                        key=lambda k: candidate_models_similarity_list[k],
+                                        reverse=True)[:k]
 
-    return topk_model_info_list, topk_model_accuracy_list
+    return similarity_sorted_idx_list
 
 
-def estimate_model_accuracy(input_model, topk_model_info_list, topk_model_accuracy_list):
+def estimate_model_accuracy(input_model_epoch, predict_model_epoch_list, predict_model_accuracy_list):
 
-    def poisson(k, lamb):
-        # poisson pdf, parameter lamb is the fit parameter
-        return (lamb ** k / factorial(k)) * np.exp(-lamb)
+    popt, pcov = curve_fit(sigmoid_function,
+                           predict_model_epoch_list,
+                           predict_model_accuracy_list,
+                           method='dogbox')
 
-    def negative_log_likelihood(params, data):
-        # The negative log-Likelihood-Function
-        lnl = - np.sum(np.log(poisson(data, params[0])))
-        return lnl
-
-    def negative_log_likelihood(params, data):
-        # better alternative using scipy
-        return -stats.poisson.logpmf(data, params[0]).sum()
-        # return -stats.norm.logpdf(data, params[0]).sum()
-
-    topk_model_epochs_list = list()
-    for m_idx in topk_model_info_list:
-        topk_model_epochs_list.append(m_idx['epochs'])
-
-    input_model_epoch = input_model['epochs']
-
-    # minimize the negative log-Likelihood
-    result = minimize(negative_log_likelihood,  # function to minimize
-                      x0=np.asarray(topk_model_epochs_list[:1]),  # start value
-                      args=(input_model_epoch,),  # additional arguments for function
-                      method='Powell',  # minimization method, see docs
-                      )
-
-    predict_accuracy = stats.poisson.pmf(input_model_epoch, result.x)
-    print(predict_accuracy)
+    estimated_accuracy = sigmoid_function(input_model_epoch, popt[0], popt[1])
+    return estimated_accuracy
 
 
 if __name__ == "__main__":
-    input_model = '224-3-64-1-1-5-0.001-SGD-relu-18'
-    input_model_list = input_model.split('-')
+    INPUT_MODEL_INFO = '224-3-64-1-1-5-0.001-SGD-relu-18'
+    input_model_list = INPUT_MODEL_INFO.split('-')
     input_model_dict = dict()
 
     input_model_dict['input_size'] = int(input_model_list[0])
@@ -133,11 +114,17 @@ if __name__ == "__main__":
     input_model_dict['learning_rate'] = float(input_model_list[6])
     input_model_dict['optimizer'] = input_model_list[7]
     input_model_dict['activation'] = input_model_list[8]
-    input_model_dict['epochs'] = int(input_model_list[9])
 
-    conv_model_info_list, conv_model_accuracy_list = generate_conv_model_dataset()
-    conv_model_similarity_list = compute_model_similarity(input_model_dict, conv_model_info_list)
-    topk_model_info_list, topk_model_accuracy_list = rank_model_similarity(conv_model_info_list, conv_model_accuracy_list, conv_model_similarity_list)
+    input_model_epoch = int(input_model_list[9])
 
-    estimate_model_accuracy(input_model_dict, topk_model_info_list, topk_model_accuracy_list)
+    model_info_list, model_accuracy_list, model_epoch_list = generate_conv_model_dataset()
+    model_similarity_list = compute_model_similarity(input_model_dict, model_info_list)
+    similarity_model_idx_list = rank_model_similarity(model_info_list, model_similarity_list)
 
+    # return the nearest models info according to similarity
+    # neighbor_model_info_list = [model_info_list[i] for i in similarity_model_idx_list]
+    neighbor_model_accuracy_list = [model_accuracy_list[i] for i in similarity_model_idx_list]
+    neighbor_model_epoch_list = [model_epoch_list[i] for i in similarity_model_idx_list]
+    estimated_accuracy = estimate_model_accuracy(input_model_epoch, neighbor_model_epoch_list, neighbor_model_accuracy_list)
+
+    print(estimated_accuracy)
