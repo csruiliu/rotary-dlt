51392a7789141a6b71637bff8e632e143d4e1a73
csruiliu <rui.liu@gmx.com>
2020-08-29 20:13:25 -0500

    update resnet[3]

--- ml-sched/model_resnet.py
+++ ml-sched/model_resnet.py
@@ -167,24 +167,25 @@ class resnet(object):
 
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=batch_labels, logits=logits)
         cross_entropy_cost = tf.reduce_mean(cross_entropy)
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope=self.net_name + '_instance')
-        with tf.control_dependencies(update_ops):
-            if self.opt == 'Adam':
-                self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(cross_entropy_cost)
-            elif self.opt == 'SGD':
-                self.train_op = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(cross_entropy_cost)
-            elif self.opt == 'Adagrad':
-                self.train_op = tf.train.AdagradOptimizer(self.learning_rate).minimize(cross_entropy_cost)
-            elif self.opt == 'Momentum':
-                self.train_op = tf.train.MomentumOptimizer(self.learning_rate, 0.9).minimize(cross_entropy_cost)
+        reg_loss = tf.losses.get_regularization_loss()
+        train_loss = cross_entropy_cost + reg_loss
+
+        if self.opt == 'Adam':
+            self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(train_loss)
+        elif self.opt == 'SGD':
+            self.train_op = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(train_loss)
+        elif self.opt == 'Adagrad':
+            self.train_op = tf.train.AdagradOptimizer(self.learning_rate).minimize(train_loss)
+        elif self.opt == 'Momentum':
+            self.train_op = tf.train.MomentumOptimizer(self.learning_rate, 0.9).minimize(train_loss)
 
         return self.train_op
 
     def evaluate(self, logits, eval_labels):
         prediction = tf.equal(tf.argmax(logits, -1), tf.argmax(eval_labels, -1))
-        self.train_op = tf.reduce_mean(tf.cast(prediction, tf.float32))
+        self.eval_op = tf.reduce_mean(tf.cast(prediction, tf.float32))
 
-        return self.train_op
+        return self.eval_op
 
     def get_residual_layer(self):
         if self.residual_layer == 18:
