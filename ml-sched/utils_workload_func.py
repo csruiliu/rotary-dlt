8603c8f4b3664a8e887e74b2d2be2ff9da3e722d
csruiliu <rui.liu@gmx.com>
2020-09-01 23:14:29 -0500

    add num of model layer for utils_workload_func

--- ml-sched/utils_workload_func.py
+++ ml-sched/utils_workload_func.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 
-def generate_workload(job_num, model_type_set, batch_size_set, optimizer_set, learn_rate_set, activation_set, train_data):
+def generate_workload(job_num, model_type_set, batch_size_set, optimizer_set, learn_rate_set,
+                      activation_set, train_data):
+
     sampled_model_type_list = np.random.choice(model_type_set, job_num, replace=True)
     sampled_batch_size_list = np.random.choice(batch_size_set, job_num, replace=True)
     sampled_optimizer_list = np.random.choice(optimizer_set, job_num, replace=True)
@@ -14,13 +16,17 @@ def generate_workload(job_num, model_type_set, batch_size_set, optimizer_set, le
         sch_model_config_dict = dict()
         sch_model_config_dict['job_id'] = i
         sch_model_config_dict['model_type'] = sampled_model_type_list[i]
+        if sch_model_config_dict['model_type'] == 'resnet':
+            sch_model_config_dict['model_layer_num'] = np.random.choice([18, 34, 50, 101, 152], replace=False)
+        elif sch_model_config_dict['model_type'] == 'densenet':
+            sch_model_config_dict['model_layer_num'] = np.random.choice([121, 169, 201, 264], replace=False)
+        else:
+            sch_model_config_dict['model_layer_num'] = np.random.randint(1, 10)
         sch_model_config_dict['batch_size'] = sampled_batch_size_list[i]
         sch_model_config_dict['optimizer'] = sampled_optimizer_list[i]
         sch_model_config_dict['learning_rate'] = sampled_learning_rate_list[i]
         sch_model_config_dict['activation'] = sampled_activation_list[i]
         sch_model_config_dict['train_dataset'] = train_data
-        #sch_model_config_dict['cur_accuracy'] = 0
-        #sch_model_config_dict['prev_accuracy'] = 0
         sch_workload.append(sch_model_config_dict)
 
     return sch_workload
