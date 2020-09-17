2491f9d5535912fa6c3917e817590bd194adfdf9
csruiliu <rui.liu@gmx.com>
2020-09-16 23:51:24 -0500

    update accuracy profiler

--- ml-sched/profiler_accuracy.py
+++ ml-sched/profiler_accuracy.py
@@ -34,11 +34,12 @@ def train_eval_model(trainOp, evalOp, model_info):
         config.gpu_options.allow_growth = True
 
         if train_data == 'imagenet':
-            image_list = sorted(os.listdir(imagenet_train_img_path))
+            image_list_train = sorted(os.listdir(imagenet_train_img_path))
+            image_list_eval = sorted(os.listdir(imagenet_test_img_path))
 
         with tf.Session(config=config) as sess:
             sess.run(tf.global_variables_initializer())
-            num_batch = Y_data.shape[0] // train_batchsize
+            num_batch = Y_data_train.shape[0] // train_batchsize
 
             for e in range(train_epoch):
                 for i in range(num_batch):
@@ -47,14 +48,15 @@ def train_eval_model(trainOp, evalOp, model_info):
                     batch_end = (i+1) * train_batchsize
 
                     if train_data == 'imagenet':
-                        batch_list = image_list[batch_offset:batch_end]
-                        X_mini_batch_feed = load_imagenet_raw(imagenet_test_img_raw_path, batch_list, img_height, img_width)
+                        batch_list = image_list_train[batch_offset:batch_end]
+                        X_mini_batch_feed = load_imagenet_raw(imagenet_train_img_path, batch_list, img_height, img_width)
                     else:
                         X_mini_batch_feed = X_data[batch_offset:batch_end]
 
-                    Y_mini_batch_feed = Y_data[batch_offset:batch_end]
+                    Y_mini_batch_feed = Y_data_train[batch_offset:batch_end]
                     sess.run(trainOp, feed_dict={features: X_mini_batch_feed, labels: Y_mini_batch_feed})
 
+            X_data_eval = load_imagenet_raw(imagenet_test_img_path, image_list_eval, img_height, img_width)
             acc_arg = sess.run(evalOp, feed_dict={features: X_data_eval, labels: Y_data_eval})
 
     print("{{\"model_name\": \"{}\", \"model_accuracy\": {}}}".format(model_info, acc_arg))
@@ -92,6 +94,9 @@ if __name__ == '__main__':
                         choices=['resnet', 'mobilenet', 'densenet', 'mlp', 'scn'],
                         help='model type [resnet, mobilenet, mlp, densenet, scn]')
 
+    parser.add_argument('-l', '--layer', required=True, action='store', type=int,
+                        help='the number of layer decides a model, for example, the layer is 50 for resnet-50')
+
     parser.add_argument('-b', '--batchsize', required=True, action='store', type=int,
                         help='batch size, for example: 32, 50, 100]')
 
@@ -111,22 +116,20 @@ if __name__ == '__main__':
                         choices=['relu', 'sigmoid', 'tanh', 'leaky_relu'],
                         help='activation, for example, relu, sigmoid, tanh, leaky_relu')
 
-    parser.add_argument('-l', '--layer', action='store', type=int,
-                        help='the number of layer decides a model, for example, the layer is 50 for resnet-50')
-
     parser.add_argument('-d', '--device', action='store', type=str, default='gpu:0', choices=['gpu:0', 'gpu:1'],
                         help='select a device to run device')
 
     args = parser.parse_args()
 
-    #if args.model not in ['mlp', 'scn'] and args.conv_layer:
-    #    train_conv_layer = args.conv_layer
-    #else:
-    #    train_conv_layer = 1
+    if args.model in ['resnet'] and args.conv_layer not in [18, 34, 50, 101, 152]:
+        raise ValueError('number of layers are not supported in ResNet')
+    elif args.model in ['densenet'] and args.conv_layer not in [121, 169, 201, 264]:
+        raise ValueError('number of layers are not supported in DenseNet')
+    else:
+        train_layer = args.layer
 
     train_data = args.train_set
     train_model = args.model
-    train_layer = args.layer
     train_batchsize = args.batchsize
     train_epoch = args.epoch
     train_opt = args.opt
@@ -151,13 +154,10 @@ if __name__ == '__main__':
 
         imagenet_train_img_path = cfg_path_yml.imagenet_t50k_img_raw_path
         imagenet_train_label_path = cfg_path_yml.imagenet_t50k_img_label_path
-
-        imagenet_test_img_raw_path = cfg_path_yml.imagenet_t1k_img_raw_path
-        imagenet_test_img_bin_path = cfg_path_yml.imagenet_t1k_img_bin_path
+        imagenet_test_img_path = cfg_path_yml.imagenet_t1k_img_raw_path
         imagenet_test_label_path = cfg_path_yml.imagenet_t1k_label_path
 
-        Y_data = load_imagenet_labels_onehot(imagenet_train_label_path, num_classes)
-        X_data_eval = load_imagenet_bin(imagenet_test_img_bin_path, num_channels, img_width, img_height)
+        Y_data_train = load_imagenet_labels_onehot(imagenet_train_label_path, num_classes)
         Y_data_eval = load_imagenet_labels_onehot(imagenet_test_label_path, num_classes)
 
     elif train_data == 'cifar10':
