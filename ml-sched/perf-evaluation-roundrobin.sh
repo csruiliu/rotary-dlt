6290e6592e09241a4db5091e1393a993beb2f893
csruiliu <rui.liu@gmx.com>
2020-09-22 21:43:57 -0500

    add clean step

--- ml-sched/perf-evaluation-roundrobin.sh
+++ ml-sched/perf-evaluation-roundrobin.sh
@@ -4,13 +4,14 @@ JOB_LIST="10 50 100 150 200"
 TIME_SLOT="48 72 96 120"
 for jidx in ${JOB_LIST}
 do
+  rm -rf __pycache__
+  python3 clean_gpu_cache.py
   python3 schedule_evaluation_roundrobin.py -j ${jidx} >> ./EXP-JOB${jidx}.txt
 done
 
-rm -rf __pycache__
-python3 clean_gpu_cache.py
-
 for tidx in ${TIME_SLOT}
 do
+  rm -rf __pycache__
+  python3 clean_gpu_cache.py
   python3 schedule_evaluation_roundrobin.py -t ${tidx} >> ./EXP-TIME${tidx}.txt
 done
