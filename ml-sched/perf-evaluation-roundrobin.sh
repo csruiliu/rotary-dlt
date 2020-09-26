94cdfa5b5b5e02a1b22ed283196c28c21d8a6af3
csruiliu <rui.liu@gmx.com>
2020-09-26 17:28:33 -0500

    update evaluation script

--- ml-sched/perf-evaluation-roundrobin.sh
+++ ml-sched/perf-evaluation-roundrobin.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-JOB_LIST="20 40 60 80 100"
+JOB_LIST="20 50 80 110 140"
 TIME_SLOT="48 60 72 84 96"
 for jidx in ${JOB_LIST}
 do
   rm -rf __pycache__
   python3 clean_gpu_cache.py
-  python3 schedule_evaluation_roundrobin.py -j ${jidx} >> ./EXP-JOB${jidx}.txt
+  python3 evaluation_roundrobin.py -j ${jidx} >> ./EXP-JOB${jidx}.txt
 done
 
 for tidx in ${TIME_SLOT}
 do
   rm -rf __pycache__
   python3 clean_gpu_cache.py
-  python3 schedule_evaluation_roundrobin.py -t ${tidx} >> ./EXP-TIME${tidx}.txt
+  python3 evaluation_roundrobin.py -t ${tidx} >> ./EXP-TIME${tidx}.txt
 done
