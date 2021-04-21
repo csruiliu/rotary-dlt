4c9c55da0c9db5d172d10828ad9edba12a7dd721
csruiliu <rui.liu@gmx.com>
2021-04-21 11:07:12 -0500

    reconstructure

--- rl_schedule.py
+++ rl_schedule.py
@@ -1,20 +1,261 @@
 import tensorflow as tf
 from multiprocessing import Process, Manager
 from timeit import default_timer as timer
+import numpy as np
 import os
 
-import relish.config.config_parameter as cfg_para
-import relish.config.config_path as cfg_path
-from relish.core.relish_environment import SchedEnv
-from relish.core.relish_engine import SchedEngine
-from relish.common.model_importer import ModelImporter
-from relish.common.dataset_loader import load_dataset_para, load_train_dataset, load_eval_dataset
-from relish.tools.img_tool import load_imagenet_raw
-from relish.tools.workload_func import generate_workload_slo
-from relish.estimator.estimator_model_accuracy import AccuracyEstimator
-from relish.estimator.estimator_model_steptime_multidevices import MultiDeviceTimeEstimator
+from tf_agents.agents.ppo import ppo_agent
+from tf_agents.networks import actor_distribution_network
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+import tf_agents.specs.tensor_spec as tensor_spec
+from tf_agents.trajectories import time_step as ts
+
+from utils.img_tool import load_imagenet_raw
+from utils.workload_func import generate_workload_slo
+from estimator.accuracy_estimator import AccuracyEstimator
+import config.config_parameter as cfg_para
+import config.config_path as cfg_path
 
 tf.compat.v1.enable_v2_behavior()
+
+
+class RLEngine:
+    def __init__(self, sch_env, reward_function):
+        self._mlsch_env = sch_env
+        self._rl_agent = None
+        self._replay_buffer = None
+        self._reward_list = list()
+        self._reward_func = reward_function
+
+    def build_sch_agent(self):
+
+        actor_net = actor_distribution_network.ActorDistributionNetwork(self._mlsch_env.observation_spec(),
+                                                                        self._mlsch_env.action_spec(),
+                                                                        fc_layer_params=(100,))
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        train_step_counter = tf.Variable(0)
+
+        self._rl_agent = ppo_agent.PPOAgent(self._mlsch_env.time_step_spec(), self._mlsch_env.action_spec(),
+                                            actor_net=actor_net, optimizer=optimizer, normalize_rewards=True,
+                                            train_step_counter=train_step_counter)
+
+        self._rl_agent.initialize()
+        self._rl_agent.train = common.function(self._rl_agent.train)
+        self._rl_agent.train_step_counter.assign(0)
+
+    def train_sch_agent(self, num_train_iterations, collect_episodes_per_iteration, steps_num_per_batch,
+                        log_interval, include_eval, eval_interval, num_eval_episodes_for_train):
+        self._init_replay_buffer(steps_num_per_batch)
+
+        for i in range(num_train_iterations):
+            print('training iteration {0}'.format(i))
+            # Collect a few step using collect_policy and save to the replay buffer.
+            self._replay_collect_episode(collect_episodes_per_iteration)
+
+            experience = self._replay_buffer.gather_all()
+            train_loss = self._rl_agent.train(experience)
+            self._replay_buffer.clear()
+
+            step = self._rl_agent.train_step_counter.numpy()
+
+            if step % log_interval == 0:
+                print('step = {0}: loss = {1}'.format(step, train_loss.loss))
+
+            if include_eval and step % eval_interval == 0:
+                reward = self.evaluate_sch_agent(num_eval_episodes_for_train)
+                print('evaluate the agent at step = {0}: Average Return = {1}'.format(step, reward))
+                self._reward_list.append(reward)
+
+        return self._reward_list
+
+    def evaluate_sch_agent(self, eval_num_episodes):
+        print('evaluate the agent for {} episodes.'.format(eval_num_episodes))
+        episode_return_list = list()
+
+        for _ in range(eval_num_episodes):
+            time_step = self._mlsch_env.reset()
+
+            while not time_step.is_last():
+                action_step = self._rl_agent.policy.action(time_step)
+                time_step = self._mlsch_env.step(action_step.action)
+                episode_return_list.append(time_step.reward)
+
+        # avg_reward = total_return / eval_num_episodes
+        print('episode_return_list', episode_return_list)
+        print('###############################')
+        reward = getattr(self._reward_func, self._mlsch_env.evaluation_function())(episode_return_list)
+
+        return reward
+
+    def generate_schedule(self):
+        print('generate the schedule...')
+        sch_action_list = list()
+        time_step = self._mlsch_env.reset()
+        while not time_step.is_last():
+            action_step = self._rl_agent.policy.action(time_step)
+            sch_action_list.append(action_step.action.numpy())
+            time_step = self._mlsch_env.step(action_step.action)
+        return sch_action_list
+
+    def benchmark_before_training(self, benchmark_num_episodes=10):
+        # Evaluate the agent's policy once before training.
+        avg_return = self.evaluate_sch_agent(benchmark_num_episodes)
+        self._reward_list.append(avg_return)
+        print('Returns before training:{}'.format(self._reward_list))
+
+    def _init_replay_buffer(self, steps_num_per_batch):
+        self._replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(data_spec=self._rl_agent.collect_data_spec,
+                                                                             batch_size=self._mlsch_env.batch_size(),
+                                                                             max_length=steps_num_per_batch)
+
+    def _replay_collect_episode(self, collect_num_episodes):
+        episode_counter = 0
+
+        while episode_counter < collect_num_episodes:
+            current_time_step = self._mlsch_env.current_time_step()
+            action_step = self._rl_agent.collect_policy.action(current_time_step)
+            next_time_step = self._mlsch_env.step(action_step.action)
+            traj = trajectory.from_transition(current_time_step, action_step, next_time_step)
+
+            # Add trajectory to the replay buffer
+            if self._replay_buffer is None:
+                raise TypeError('replay buffer has not been initialize')
+
+            self._replay_buffer.add_batch(traj)
+
+            if traj.is_boundary():
+                self._mlsch_env.reset()
+                episode_counter += 1
+
+    def _replay_collect_step(self, environment, policy, num_steps):
+        step_counter = 0
+
+        while step_counter < num_steps:
+            current_time_step = environment.current_time_step()
+            action_step = policy.action(current_time_step)
+            next_time_step = environment.simulated_step(action_step.action)
+            traj = trajectory.from_transition(current_time_step, action_step, next_time_step)
+
+            # Add trajectory to the replay buffer
+            self._replay_buffer.add_batch(traj)
+
+            step_counter += 1
+
+    def reset(self):
+        self._rl_agent = None
+        self._replay_buffer = None
+        self._reward_list.clear()
+
+
+class RLEnv:
+    def __init__(self,
+                 time_slots_num,
+                 gpu_device_num,
+                 cpu_device_num,
+                 workload,
+                 reward_function,
+                 is_simulation=False):
+
+        self._time_slots_num = time_slots_num
+        self._gpu_device_num = gpu_device_num
+        self._cpu_device_num = cpu_device_num
+        self._workload = workload
+        self._workload_size = len(workload)
+        self._total_device_num = self._gpu_device_num + self._cpu_device_num
+        # gpu idx: from 0 to gpu_device_num-1, cpu idx: from cpu_device_num to total_device_num-1
+        self._action_spec = tensor_spec.BoundedTensorSpec(self._total_device_num, dtype=tf.int32, minimum=1,
+                                                          maximum=self._workload_size, name='action')
+        # observation: the accuracy and overhead of each job in the workload
+        self._observation_spec = tensor_spec.TensorSpec(self._workload_size, dtype=tf.float32)
+        self._time_step_spec = ts.time_step_spec(self._observation_spec)
+        self._reward_function = reward_function
+        self._evaluation_function = reward_function + '_evaluation'
+        self._is_simulation = is_simulation
+
+        self._current_time_step = None
+        # accuracy is workload-oriented so the shape of accuracy array is based on self._workload_size
+        self._accuracy_array = np.zeros(self._workload_size, dtype=np.float32)
+        # steptime array records the steptime of jobs on each devices, each device has only one job
+        self._steptime_array = np.zeros(shape=(self._time_slots_num, self._total_device_num), dtype=np.float32)
+        self._episode_ended = False
+        self._assigned_time_slots_num = 0
+        self._batch_size = 1
+
+        self._steptime_estimator = None
+        self._accuracy_estimator = None
+
+    def step(self, action):
+        if self._episode_ended:
+            return self.reset()
+
+        if self._is_simulation:
+            for gidx in range(self._gpu_device_num):
+                job_idx = action[gidx]
+                self._accuracy_array[job_idx] += np.random.uniform(0, 0.1, 1)
+                self._steptime_array[self._assigned_time_slots_num][gidx] += np.random.uniform(0, 0.8, 1)
+
+            for cidx in range(self._gpu_device_num, self._total_device_num):
+                job_idx = action[cidx]
+                self._accuracy_array[job_idx] += np.random.uniform(0, 0.1, 1)
+                self._steptime_array[self._assigned_time_slots_num][cidx] += np.random.uniform(0, 0.8, 1)
+
+        else:
+            for gidx in range(self._gpu_device_num):
+                job_idx = action[gidx]
+                #self._observation_array[job_idx][] += self._accuracy_estimator.predict_gpu_steptime(workload)
+
+        self._assigned_time_slots_num += 1
+
+        # use award function
+        reward = getattr(self._reward_function)(self._accuracy_array,
+                                                self._steptime_array[:self._gpu_device_num],
+                                                self._steptime_array[self._gpu_device_num:self._total_device_num])
+
+        if self._assigned_time_slots_num == self._time_slots_num:
+            self._current_time_step = ts.termination(self._accuracy_array, reward)
+            self._episode_ended = True
+            return self._current_time_step
+        else:
+            self._current_time_step = ts.transition(self._accuracy_array, reward)
+            return self._current_time_step
+
+    def load_estimator(self, mte, ae):
+        self._steptime_estimator = mte
+        self._accuracy_estimator = ae
+
+    def current_time_step(self):
+        return self._current_time_step
+
+    def action_spec(self):
+        return self._action_spec
+
+    def observation_spec(self):
+        return self._observation_spec
+
+    def time_step_spec(self):
+        return self._time_step_spec
+
+    def set_batch_size(self, batch_size):
+        self._batch_size = batch_size
+
+    def batch_size(self):
+        return self._batch_size
+
+    def reset(self):
+        self._episode_ended = False
+        self._assigned_time_slots_num = 0
+        self._accuracy_array.fill(0)
+        self._steptime_array.fill(0)
+        self._current_time_step = ts.restart(np.zeros(self._time_step_spec.observation.shape, dtype=np.float32))
+        return self._current_time_step
+
+    def reward_function(self):
+        return self._reward_function
+
+    def evaluation_function(self):
+        return self._evaluation_function
 
 
 def produce_job_roundrobin(sch_workload_use):
@@ -133,9 +374,12 @@ def schedule_job_rlsched(sch_workload, sch_reward_func):
     cpu_device_num = cfg_para.sch_cpu_num
 
     # init schedule environment
-    mlsch_env = SchedEnv(time_slots_num, gpu_device_num,
-                         cpu_device_num, sch_workload,
-                         sch_reward_func, is_simulation=False)
+    mlsch_env = RLEnv(time_slots_num,
+                      gpu_device_num,
+                      cpu_device_num,
+                      sch_workload,
+                      sch_reward_func,
+                      is_simulation=False)
 
     # Get path parameters from config
     steptime_dataset_path = cfg_path.multidevices_time_dataset_path
@@ -151,7 +395,7 @@ def schedule_job_rlsched(sch_workload, sch_reward_func):
 
     mlsch_env.load_estimator(mlsch_mte, mlsch_ae)
 
-    mlsch_engine = SchedEngine(mlsch_env)
+    mlsch_engine = RLEngine(mlsch_env)
 
     mlsch_engine.build_sch_agent()
     mlsch_engine.benchmark_before_training(benchmark_num_episodes=20)
